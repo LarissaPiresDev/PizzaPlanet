b9f8bf4e740bf5578c3f6e7f80817150c6cf81f0
Wender1903 <wenderman74@gmail.com>
2025-10-08 22:52:22 -0300

    :sparkles: feat: Adicionando rota post para cliente

--- restaurante/routes/cliente_routes.py
+++ restaurante/routes/cliente_routes.py
@@ -1,11 +1,29 @@
 from flask import Blueprint, request, jsonify
 from models import Cliente
+from config import db
+from schemas import ClienteSchema
 
 cliente_bp = Blueprint('cliente_bp', __name__)
+
+cliente_schema = ClienteSchema()
+clientes_schema = ClienteSchema(many=True)
 
 @cliente_bp.route('/clientes', methods=['GET'])
 def listar_clientes():
     clientes = Cliente.query.all()
-    return jsonify([{"id": c.id, "nome": c.nome, "cpf": c.cpf} for c in clientes])
+    return clientes_schema.jsonify(clientes)
+
+@cliente_bp.route('/clientes', methods=['POST'])
+def criar_cliente():
+    data = request.get_json()
+    try:
+        cliente = cliente_schema.load(data)
+        db.session.add(cliente)
+        db.session.commit()
+        return cliente_schema.jsonify(cliente), 201
+    except Exception as e:
+        db.session.rollback()
+        return jsonify({"erro": str(e)}), 400
+
 
 
