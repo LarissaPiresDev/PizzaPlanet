afdc5a6f69fbf0a826ad536d84d6f5bd7f0e50c5
Wender1903 <wenderman74@gmail.com>
2025-10-08 23:32:50 -0300

    :sparkles: feat: Adicionando Método GET por ID  para entidade ItemCardapio

--- restaurante/routes/itemcardapio_routes.py
+++ restaurante/routes/itemcardapio_routes.py
@@ -1,10 +1,20 @@
 from flask import Blueprint, request, jsonify
 from models import ItemCardapio
-
+from config import db
+from schemas import ItemCardapioSchema
 
 itemcardapio_bp = Blueprint('itemcardapio_bp', __name__)
 
 @itemcardapio_bp.route('/itemcardapio', methods=['GET'])
 def listar_itens():
     itens = ItemCardapio.query.all()
-    return jsonify([{"id": i.id, "nome": i.nome, "preco": i.preco} for i in itens])
+    schema = ItemCardapioSchema(many=True)
+    return jsonify(schema.dump(itens))
+
+@itemcardapio_bp.route('/itemcardapio/<int:id>', methods=['GET'])
+def listar_item_por_id(id):
+    item = ItemCardapio.query.get(id)
+    if not item:
+        return jsonify({"erro": "Item não encontrado"}), 404
+    schema = ItemCardapioSchema()
+    return jsonify(schema.dump(item))
