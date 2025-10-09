3057398b8193b7b2150184606c7ea84a0889a705
Wender1903 <wenderman74@gmail.com>
2025-10-08 23:27:08 -0300

    :sparkles: feat: Adicionando Método de listar por id para entidade de Funcionário

--- restaurante/routes/funcionario_routes.py
+++ restaurante/routes/funcionario_routes.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, request, jsonify
 from models import Funcionario
+from config import db
+from schemas import FuncionarioSchema
 
 funcionario_bp = Blueprint('funcionario_bp', __name__)
 
@@ -7,4 +9,11 @@ funcionario_bp = Blueprint('funcionario_bp', __name__)
 def listar_funcionarios():
     funcionarios = Funcionario.query.all()
     return jsonify([{"id": f.id, "nome": f.nome} for f in funcionarios])
+
+@funcionario_bp.route('/funcionarios/<int:id>', methods=['GET'])
+def listar_funcionario_por_id(id):
+    funcionario = Funcionario.query.get(id)
+    if not funcionario:
+        return jsonify({"erro": "Funcionário não foi encontrado"}), 404
+    return jsonify({"id": funcionario.id, "nome": funcionario.nome, "cpf": funcionario.cpf})
 
