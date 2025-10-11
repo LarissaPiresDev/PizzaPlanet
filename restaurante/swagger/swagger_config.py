54fb7bdf9bc98a86b436b0652f7c1d390b8d455e
Wender1903 <wenderman74@gmail.com>
2025-10-11 17:53:48 -0300

    :sparkles: feat: Adicionando Documentação completa para a entidade Item Cardápio

--- restaurante/swagger/swagger_config.py
+++ restaurante/swagger/swagger_config.py
@@ -1,10 +1,12 @@
 from . import api
 from swagger.namespace.cliente_namespace import clientes_ns
 from swagger.namespace.funcionario_namespace import funcionarios_ns
+from swagger.namespace.itemcardapio_namespace import itens_ns
 
 def configure_swagger(app):
     """Configura a documentação Swagger e registra os namespaces"""
     api.init_app(app)
-    api.add_namespace(clientes_ns, path="/clientes")
-    api.add_namespace(funcionarios_ns, path="/funcionarios")
+    api.add_namespace(clientes_ns, path="/cliente")
+    api.add_namespace(funcionarios_ns, path="/funcionario")
+    api.add_namespace(itens_ns, path="/itemcardapio")
     api.mask_swagger = False
