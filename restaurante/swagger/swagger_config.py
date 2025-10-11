3742e3fe5aebf3309b8835e442e4c1fda75b6bf6
Wender1903 <wenderman74@gmail.com>
2025-10-11 17:50:03 -0300

    :sparkles: feat: Adicionando Documentação completa para a entidade Funcionário

--- restaurante/swagger/swagger_config.py
+++ restaurante/swagger/swagger_config.py
@@ -1,8 +1,10 @@
 from . import api
 from swagger.namespace.cliente_namespace import clientes_ns
+from swagger.namespace.funcionario_namespace import funcionarios_ns
 
 def configure_swagger(app):
     """Configura a documentação Swagger e registra os namespaces"""
     api.init_app(app)
-    api.add_namespace(clientes_ns, path="/cliente")
+    api.add_namespace(clientes_ns, path="/clientes")
+    api.add_namespace(funcionarios_ns, path="/funcionarios")
     api.mask_swagger = False
