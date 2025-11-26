cffcc9247f7bfe8e5d442fb0bf66575a51de4e92
Wender1903 <wenderman74@gmail.com>
2025-11-26 05:34:27 -0300

    :recycle: refactor: retira arquivos de rotas de cliente e funcionario e adicionand CORS para front end

--- restaurante/app.py
+++ restaurante/app.py
@@ -1,17 +1,14 @@
 from flask_sqlalchemy import SQLAlchemy
-from routes.cliente_routes import cliente_bp
 from config import app, db
+from flask_cors import CORS   
 
-from routes.cliente_routes import cliente_bp
-from routes.funcionario_routes import funcionario_bp
 from routes.itemcardapio_routes import itemcardapio_bp
 from routes.itempedido_routes import itempedido_bp
 from routes.pedido_routes import pedido_bp
 
+# Habilitar CORS para toda a API
+CORS(app, resources={r"/*": {"origins": "*"}}) 
 
-
-app.register_blueprint(cliente_bp)
-app.register_blueprint(funcionario_bp)
 app.register_blueprint(itemcardapio_bp)
 app.register_blueprint(itempedido_bp)
 app.register_blueprint(pedido_bp)
@@ -26,5 +23,4 @@ with app.app_context():
     db.create_all()
 
 if __name__ == '__main__':
-    app.run(host=app.config["HOST"], port = app.config['PORT'],debug=app.config['DEBUG']) 
-
+    app.run(host=app.config["HOST"], port=app.config['PORT'], debug=app.config['DEBUG'])
